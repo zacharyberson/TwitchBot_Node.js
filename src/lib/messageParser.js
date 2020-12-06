271dc7152c5a542e9877f1f0a95b6f4bdad73cd6
zacharyberson <zacharyberson@gmail.com>
2020-12-05 19:24:22 -0500

    feature: added ability to routinely print messages to chat feature: added ability to have hidden commands not listed in chat chore: removed unused delegator

---
+++ src/lib/messageParser.js
@@ -0,0 +1,48 @@
+const { commands, hiddenCommands, regex } = require('./commands.js');
+
+const processCommand = (target, context, command) => {
+    const commandParts = command.split(' ', 2);
+    const commandName = commandParts[0];
+    const commandQuery = commandParts.length > 1 ? commandParts[1] : '';
+
+    // try to match first word to known command
+    Object.keys(commands).forEach((value) => {
+        if (commandName === value) {
+            try {
+                commands[value](target, context, commandQuery);
+                console.log(`* Executed ${value} command`);
+                return;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+
+    // try to match first word to known command
+    Object.keys(hiddenCommands).forEach((value) => {
+        if (commandName === value) {
+            try {
+                hiddenCommands[value](target, context, commandQuery);
+                console.log(`* Executed ${value} command`);
+                return;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+
+    // try to match entire string to known command regex
+    Object.keys(regex).forEach((value) => {
+        if (command.match(new RegExp(value))) {
+            try {
+                regex[value](target, context, command);
+                console.log(`* Matched ${value} regex`);
+                return;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+}
+
+exports.processCommand = processCommand;
